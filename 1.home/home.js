905fff0d69e1873e2a9c0c2109b7918a1d7b63fa
Valentina Nies <valunies@gmail.com>
2019-11-08 13:08:35 -0300

    home hover

--- 1.home/home.js
+++ 1.home/home.js
@@ -20,72 +20,150 @@ fetch(URL_POPULARES)
     var li = ""
     for (var i = 0; i < arrayDeSeries.length; i++) {
 
-      li = '<li>'
-      li +=    '<div class="uk-card uk-card-default uk-card-body">' + arrayDeSeries[i].name + '</div>'
+      li = '<li class="contenedorDeSerie">'
+      li +=    '<p class="tituloDeSerie">' + arrayDeSeries[i].name + '</p>'
       li +=     '<img src="'+ URL_IMAGEN_FIJA + arrayDeSeries[i].poster_path + '" alt="">'
+      li +=    '<p class="vote">' + arrayDeSeries[i].vote_average + '</p>'
       li += '</li>'
 
       ul.innerHTML += li
     }
 
+    var contenedoresDeSeriesPopulares = document.querySelectorAll("#populares .contenedorDeSerie")
 
-  });
+    for (var i = 0; i < contenedoresDeSeriesPopulares.length; i++) {
+      contenedoresDeSeriesPopulares[i].onmouseover = function() {
+        var titulo = this.querySelector(".tituloDeSerie");
+        titulo.style.display = "block";
+        titulo.classList.add("uk-animation-slide-bottom")
 
-// 3) al aire hoy
+        var vote = this.querySelector(".vote");
+        vote.style.display = "block";
+        vote.classList.add("uk-animation-slide-bottom")
+      }
 
-var URL_AIRE_HOY = "https://api.themoviedb.org/3/tv/airing_today?api_key="+ API_KEY +"&language=en-US"
+      contenedoresDeSeriesPopulares[i].onmouseout = function() {
+        var titulo = this.querySelector(".tituloDeSerie");
+        titulo.style.display = "none";
+        titulo.classList.remove("uk-animation-slide-bottom")
 
-fetch(URL_AIRE_HOY)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(objetoLiteralRespuesta) {
-    console.log(objetoLiteralRespuesta);
-
-    // capturar el UL que va a contener los LI (series)
-    var ul = document.querySelector("#airehoy")
-    var arrayDeSeries = objetoLiteralRespuesta.results
-    var li = ""
-    for (var i = 0; i < arrayDeSeries.length; i++) {
-
-      li = '<li>'
-      li +=    '<div class="uk-card uk-card-default uk-card-body">' + arrayDeSeries[i].name + '</div>'
-      li +=     '<img src="'+ URL_IMAGEN_FIJA + arrayDeSeries[i].poster_path + '" alt="">'
-      li += '</li>'
-
-      ul.innerHTML += li
+        var vote = this.querySelector(".vote");
+        vote.style.display = "none";
+        vote.classList.add("uk-animation-slide-bottom")
+      }
     }
 
-
   });
 
-// 2) mayor puntaje
+  // 2) al aire hoy
 
-var URL_PUNTAJE = "https://api.themoviedb.org/3/tv/top_rated?api_key="+ API_KEY +"&page=1"
+  var URL_AIRE_HOY = "https://api.themoviedb.org/3/tv/airing_today?api_key="+ API_KEY +"&language=en-US"
 
-fetch(URL_PUNTAJE)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(objetoLiteralRespuesta) {
-    console.log(objetoLiteralRespuesta);
+  fetch(URL_AIRE_HOY)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(objetoLiteralRespuesta) {
+      console.log(objetoLiteralRespuesta);
 
-    // capturar el UL que va a contener los LI (series)
-    var ul = document.querySelector("#puntaje")
-    var arrayDeSeries = objetoLiteralRespuesta.results
-    var li = ""
-    for (var i = 0; i < arrayDeSeries.length; i++) {
+      // capturar el UL que va a contener los LI (series)
+      var ul = document.querySelector("#airehoy")
+      var arrayDeSeries = objetoLiteralRespuesta.results
+      var li = ""
+      for (var i = 0; i < arrayDeSeries.length; i++) {
 
-      li = '<li>'
-      li +=    '<div class="uk-card uk-card-default uk-card-body">' + arrayDeSeries[i].name + '</div>'
-      li +=     '<img src="'+ URL_IMAGEN_FIJA + arrayDeSeries[i].poster_path + '" alt="">'
-      li += '</li>'
+        li = '<li class="contenedorDeSerie">'
+        li +=    '<p class="tituloDeSerie">' + arrayDeSeries[i].name + '</p>'
+        li +=     '<img src="'+ URL_IMAGEN_FIJA + arrayDeSeries[i].poster_path + '" alt="">'
+        li +=    '<p class="vote">' + arrayDeSeries[i].vote_average + '</p>'
+        li += '</li>'
 
-      ul.innerHTML += li
-    }
+        ul.innerHTML += li
+      }
+
+      var contenedoresDeSeriesAireHoy = document.querySelectorAll("#airehoy .contenedorDeSerie")
+
+      for (var i = 0; i < contenedoresDeSeriesAireHoy.length; i++) {
+        contenedoresDeSeriesAireHoy[i].onmouseover = function() {
+          var titulo = this.querySelector(".tituloDeSerie");
+          titulo.style.display = "block";
+          titulo.classList.add("uk-animation-slide-bottom")
+
+          var vote = this.querySelector(".vote");
+          vote.style.display = "block";
+          vote.classList.add("uk-animation-slide-bottom")
+        }
+
+        contenedoresDeSeriesAireHoy[i].onmouseout = function() {
+          var titulo = this.querySelector(".tituloDeSerie");
+          titulo.style.display = "none";
+          titulo.classList.remove("uk-animation-slide-bottom")
+
+          var vote = this.querySelector(".vote");
+          vote.style.display = "none";
+          vote.classList.add("uk-animation-slide-bottom")
+        }
+      }
 
 
-  });
+
+    });
+
+
+  // 3) mayor puntaje
+
+  var URL_PUNTAJE = "https://api.themoviedb.org/3/tv/top_rated?api_key="+ API_KEY +"&page=1"
+
+  fetch(URL_PUNTAJE)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(objetoLiteralRespuesta) {
+      console.log(objetoLiteralRespuesta);
+
+      // capturar el UL que va a contener los LI (series)
+      var ul = document.querySelector("#puntaje")
+      var arrayDeSeries = objetoLiteralRespuesta.results
+      var li = ""
+      for (var i = 0; i < arrayDeSeries.length; i++) {
+
+        li = '<li class="contenedorDeSerie">'
+        li +=    '<p class="tituloDeSerie">' + arrayDeSeries[i].name + '</p>'
+        li +=     '<img src="'+ URL_IMAGEN_FIJA + arrayDeSeries[i].poster_path + '" alt="">'
+        li +=    '<p class="vote">' + arrayDeSeries[i].vote_average + '</p>'
+        li += '</li>'
+
+        ul.innerHTML += li
+      }
+
+      var contenedoresDeSeriesPuntaje = document.querySelectorAll("#puntaje .contenedorDeSerie")
+
+      for (var i = 0; i < contenedoresDeSeriesPuntaje.length; i++) {
+        contenedoresDeSeriesPuntaje[i].onmouseover = function() {
+          var titulo = this.querySelector(".tituloDeSerie");
+          titulo.style.display = "block";
+          titulo.classList.add("uk-animation-slide-bottom")
+
+          var vote = this.querySelector(".vote");
+          vote.style.display = "block";
+          vote.classList.add("uk-animation-slide-bottom")
+
+        }
+
+        contenedoresDeSeriesPuntaje[i].onmouseout = function() {
+          var titulo = this.querySelector(".tituloDeSerie");
+          titulo.style.display = "none";
+          titulo.classList.remove("uk-animation-slide-bottom")
+
+          var vote = this.querySelector(".vote");
+          vote.style.display = "none";
+          vote.classList.add("uk-animation-slide-bottom")
+
+        }
+
+      }
+
+    });
 
 
 
